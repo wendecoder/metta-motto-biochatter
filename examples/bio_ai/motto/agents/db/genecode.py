1cf43ab84faa1fa74bb6a505fd750ee97eeb6b88
hizkiastesfaye <tesfayehizkias@gmail.com>
2024-06-01 22:26:27 +0000

    add genecode funcition and edit metta_agent.py file

---
+++ examples/bio_ai/motto/agents/db/genecode.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+
+password = 'fita1234'
+name = "wenonezra258"
+cluster_address = "cluster0.jwwwq0y.mongodb.net"
+dbs_name = "genecode"
+
+uri = f"mongodb+srv://{name}:{password}@{cluster_address}/{database_name}?retryWrites=true&w=majority&appName=Cluster0"
+try:
+    client = MongoClient(uri)
+except Exception as e:
+    print(f"Error: {e}")
+db = client["gene"]
+collection = db[dbs_name]
+def Genecode(query):
+    
+    if query:
+        # print("genecode is printed")
+  
+        datas = collection.find_one({str(query[0]):str(query[1])},{'_id': 0})
+        # print("dsatass: ",datas)
+        return datas
+
+    else:
+        return (f"The gene with {query} is none")
+
+# query = ['transcript', 'ENST00000533722']
+# Genecode(query)
